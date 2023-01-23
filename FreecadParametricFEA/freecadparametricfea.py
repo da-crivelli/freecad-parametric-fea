9c4413f873c9985baa009e9cd98eac80c062419e
Davide Crivelli <da.crivelli+github@gmail.com>
2023-01-23 23:12:53 +0000

    Move FreecadModel logic to a separate file

--- FreecadParametricFEA/freecadparametricfea.py
+++ FreecadParametricFEA/freecadparametricfea.py
@@ -1,4 +1,3 @@
-import sys
 import time
 import pandas as pd
 
@@ -6,33 +5,22 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-
-def _register_freecad(freecad_path: str) -> None:
-    if freecad_path is not None and freecad_path not in sys.path:
-        sys.path.append(freecad_path)
-    # TODO: should automagically try to find freecad in the usual suspect folders;
-    # it should also automatically add /bin if the user didn't specify it
-    try:
-        global FreeCAD, femtools
-        FreeCAD = __import__("FreeCAD", globals(), locals())
-        femtools = __import__("femtools.ccxtools", globals(), locals())
-    except ImportError as err:
-        raise ImportError(
-            f"{freecad_path} does not contain the FreeCAD Python libraries"
-        ) from err
+from .freecadmodel import FreecadModel, _register_freecad
 
 
 class FreecadParametricFEA:
     def __init__(self, freecad_path: str) -> None:
         # try and find the FreeCAD path
-        _register_freecad(freecad_path=freecad_path)
+        self.freecad_path = freecad_path
 
         self.results_dataframe = pd.DataFrame()
         pass
 
     def set_model(self, freecad_document):
         if isinstance(freecad_document, str):
-            self.freecad_document = FreecadModel(freecad_document)
+            self.freecad_document = FreecadModel(
+                document_path=freecad_document, freecad_path=self.freecad_path
+            )
         elif isinstance(freecad_document, FreecadModel):
             self.freecad_document = freecad_document
 
@@ -141,62 +129,3 @@ class FreecadParametricFEA:
             self.results_dataframe.to_csv(results_filename)
         else:
             raise NotImplementedError("Export mode {} not yet implemented".format(mode))
-
-
-class FreecadModel:
-    def __init__(self, document_path: str) -> None:
-        self.filename = document_path
-        self.model = FreeCAD.open(document_path)
-        # TODO: error handling
-
-    def change_parameter(
-        self, object_name: str, constraint_name: str, target_value: float
-    ):
-        """changes a parameter (e.g. a named constraint) inside a freecad
-        document. Currently works if the constraint is inside the driving
-        sketch of the referenced object (e.g. a pocket)
-
-        Args:
-            object_name (str): name of the Freecad object containing the
-                sketch containing the constraint
-            constraint_name (str): name of the constraint to modify
-            target_value (float): target value for the constraint
-        """
-
-        target_sketch = self.model.getObject(object_name).Profile[0]
-
-        # loop over constraints to find the target
-        edge_idx = None
-        for (edge_idx, constraint) in enumerate(target_sketch.Constraints):
-            if constraint.Name == constraint_name:
-                break
-
-        # set the datum to the desired value
-        target_sketch.setDatum(edge_idx, target_value)
-
-        # apply changes and recompute
-        self.model.recompute()
-
-    def run_fea(self, solver_name: str, fea_results_name: str):
-        """runs a FEA analysis in the specified freecad document
-
-        Args:
-            solver_name (str): name of the solver (e.g. SolverCcxTools)
-            fea_results_name (str): name of the FEA results container (e.g.
-                CCX_Results)
-
-        Returns:
-            fea object: a FreeCAD object containing the FEA results
-        """
-        solver_object = self.model.getObject(solver_name)
-
-        fea = femtools.ccxtools.FemToolsCcx(solver=solver_object)
-        fea.purge_results()
-        fea.reset_all()
-        fea.update_objects()
-
-        # there should be some error handling here
-        fea.check_prerequisites()
-        fea.run()
-
-        return self.model.getObject(fea_results_name)
